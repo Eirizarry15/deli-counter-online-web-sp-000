a54eeeae1f5bea34dda912417e23fbb4fef957b3
Edgar Irizarry <eirizarry15@yahoo.com>
2018-10-10 20:07:33 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,7 +11,7 @@ def line(katz_deli)
       line_positions << "#{number}. #{customer}"
       number += 1 
     end 
-    puts "The line is currenlty: #{line_positions.join(" ")}"
+    puts "The line is currently: #{line_positions.join(" ")}"
   end
 end
 
